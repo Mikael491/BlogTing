bd7975cc529f19313ed3bf41bfbe9021b7126726
Mikael <miketekle14@gmail.com>
2016-07-06 13:22:16 -0700

    functionality for handling validation errors, update and show methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,22 +5,40 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		
+		@post = Post.new
 	end
 
 	def create
-		@post = Post.new(params.require(:post).permit(:title, :body))
+		@post = Post.new(article_params)
 
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def update
-		
+		@post = Post.find(params[:id])
+
+		if @post.update(article_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	private
+	def article_params
+		params.require(:post).permit(:title, :body)
 	end
 
 end
