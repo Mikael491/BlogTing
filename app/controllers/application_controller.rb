64af73d58ab9006c07a3670281e712f4326f561b
Mikael <miketekle14@gmail.com>
2016-07-05 20:29:16 -0700

    add root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def index
-  	redirect_to url_for(:controller => :posts, :action => :index)
-  end
+  # def index
+  # 	redirect_to url_for(:controller => :posts, :action => :index)
+  # end
 
 end
